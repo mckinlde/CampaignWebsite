3e061e99b055ac290d876c1128fd122b20525a0b
Douglas <douglas.e.mckinley@vanderbilt.edu>
2017-03-23 17:19:15 -0500

    signup.php writes to cooldemocrats database from form

--- conn_cooldemocrats.php
+++ conn_cooldemocrats.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect('localhost', 'root', '', cooldemocrats);
+$conn = mysqli_connect('localhost', 'root', '', 'cooldemocrats');
 
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
--- signup.php
+++ signup.php
@@ -1,2 +1,16 @@
 <?php
-$result
+
+include 'conn_cooldemocrats.php';
+
+$date = 'DateTime';
+$first_name = $_POST['user_first_name'];
+$last_name = $_POST['user_last_name'];
+$email = $_POST['user_mail'];
+$phone = $_POST['user_phone'];
+$register = $_POST['register'];
+$phonebank = $_POST['phonebank'];
+$canvas = $_POST['canvas'];
+
+$sql = "INSERT INTO volunteers (date, first_name, last_name, email, phone, register, phonebank, canvas)
+ VALUES ('$date', '$first_name', '$last_name', '$email', '$phone', '$register', '$phonebank', '$canvas')";
+ $result = $conn->query($sql);
